9d3434c87134fce436211341592c5316350b4ada
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-04-29 23:36:43 -0300

    app: simple change

--- App.tsx
+++ App.tsx
@@ -90,8 +90,7 @@ function App(): React.JSX.Element {
             paddingBottom: safePadding,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Changing App.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
