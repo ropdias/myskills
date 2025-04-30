81972dfd0c6bcda2afa99c97ba4d0ecb70503869
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-04-30 00:35:08 -0300

    creating Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+export function Home(){
+  return (
+    <View style={{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    }}>
+      <Text>React Native</Text>
+      <Text>Ignite</Text>
+    </View>
+  );
+}
