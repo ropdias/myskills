d21a23be740224da635ea0f213efbee45d74db90
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-05-02 00:14:16 -0300

    add greeting with useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   FlatList,
   Platform,
@@ -14,14 +14,27 @@ import { SkillCard } from '../components/SkillCard'
 export function Home() {
   const [newSkill, setNewSkill] = useState('')
   const [mySkills, setMySkills] = useState([])
+  const [greeting, setGreeting] = useState('')
 
   const handleAddNewSkill = () => {
     setMySkills((oldState) => [...oldState, newSkill])
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours()
+    if (currentHour < 12) {
+      setGreeting('Good morning')
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good afternoon')
+    } else {
+      setGreeting('Good night')
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Rodrigo</Text>
+      <Text style={styles.greetings}>{greeting}</Text>
       <TextInput
         style={styles.input}
         placeholder="New skill"
@@ -58,5 +71,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#fff',
   },
 })
